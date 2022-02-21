2fb00c2cea316403d1de69a11cfd0bf1ef8564a8
petrkucerak <petr.kucera@mainstream.cz>
2022-02-21 20:52:55 +0100

    Line format

--- 2_azure-cli/script.sh
+++ 2_azure-cli/script.sh
@@ -11,9 +11,4 @@ az account set --subscription "My Demos"
 az group create --name AzureACICLI --location northeurope
 
 # tvroba a deploy service
-az container create \
-   --resource-group AzureACICLI \
-   --name clicontainer \
-   --image mcr.microsoft.com/azuredocs/aci-helloworld \
-   --dns-name-label acidemo \
-   --ports 80
+az container create --resource-group AzureACICLI --name clicontainer --image mcr.microsoft.com/azuredocs/aci-helloworld  --dns-name-label acidemo  --ports 80
