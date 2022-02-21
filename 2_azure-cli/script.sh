cefeb29f8b309fb200d6624cfd2caad9cb743e5c
petrkucerak <petr.kucera@mainstream.cz>
2022-02-21 11:11:29 +0100

    Script for deploy ACI

---
+++ 2_azure-cli/script.sh
@@ -0,0 +1,14 @@
+# prihlaseni do azure
+az login
+
+# vypsani subscritions
+az account show --output table
+
+# nastaveni defaultni subscription
+az account set --subscription "My Demos"
+
+# tvorba RG
+az group create --name AzureACICLI --location northeurope	
+
+# tvroba a deploy service
+az container create --resource-group AzureACICLI --name clicontainer --image mcr.microsoft.com/azuredocs/aci-helloworld --dns-name-label acidemo --ports 80
